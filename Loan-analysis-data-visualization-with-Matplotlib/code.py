318a8d4c1a6d35ab3dc32002c818f2df7195c008
Umang Shah <umang.umang@gmail.com>
2019-02-19 15:51:05 +0530

    Code file created.

---
+++ Loan-analysis-data-visualization-with-Matplotlib/code.py
@@ -0,0 +1,65 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+#Code starts here
+data = pd.read_csv(path)
+loan_status = data['Loan_Status'].value_counts()
+loan_status.plot(kind='bar')
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
+ax=property_and_loan.plot(kind='bar',stacked=False,rot=45)
+ax.set(xlabel='Property Area',ylabel='Loan Status')
+
+
+# --------------
+#Code starts here
+education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
+ax= education_and_loan.plot(kind='bar',stacked=True,rot=45)
+ax.set(xlabel='Education Status',ylabel='Loan Status')
+
+
+# --------------
+#Code starts here
+graduate = data[data['Education']=='Graduate']
+not_graduate = data[data['Education']=='Not Graduate']
+graduate['LoanAmount'].plot(kind='density',label='Graduate')
+not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
+
+
+
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig, (ax_1,ax_2,ax_3) = plt.subplots(3,1)
+ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
+ax_1.set(Title='Applicant Income')
+ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
+ax_2.set(Title='Coapplicant Income')
+data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
+ax_3.set(Title ='Total Income')
+
+
